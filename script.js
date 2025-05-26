1cfd707d0f0da78c74f646d215f32f49a4bb4075
gamaliel504 <gamaalfaro969@gmail.com>
2025-05-25 18:53:48 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function hacerPedido() {
+    alert("¡Gracias por tu interés! Puedes hacer tu pedido por WhatsApp al 503 0000-0000");
+    window.open("https://wa.me/50300000000", "_blank");
+  }
+  
